b597bbb439f1e4c2a7b6b159e5651dd1d1a1ac21
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-03-11 19:05:19 -0700

    refactor: Update Python sample helpers

--- sample_code/python/helpers.py
+++ sample_code/python/helpers.py
@@ -2,6 +2,29 @@ import socket
 import subprocess
 import time
 
+def is_float(s: str) -> bool:
+    """Checks if a given string is a float.
+
+    Args:
+        s: String to test
+
+    Returns:
+        Boolean indicating whether or not the string
+        is a float.
+    """
+    r = False
+    if s.isalpha():
+        # Try to catch obvious non-floats.
+        pass
+    else:
+        try:
+            float(s)
+            r = True
+        except:
+            pass
+
+    return r
+
 
 def host_run(args: str) -> tuple:
     """Run shell commands on the host
@@ -18,7 +41,7 @@ def host_run(args: str) -> tuple:
 
     This can be useful if you are using Python primarily for the enhanced
     control flow it offers over most system shells and still want convenient
-    access to other shell features such as shell pipes, filename wildcards,
+    access to other shell features such as shell pipes, filename wild cards,
     environment variable expansion, and expansion of ~ to a user’s home
     directory.
 
@@ -44,10 +67,7 @@ def host_run(args: str) -> tuple:
 def failsafe(func) -> object:
     """Wraps functions into a try/except condition.
 
-    This allows problematic legacy functions to break without
-    interrupting the rest of the code.
-
-    Can be used as a function decorator using @failsafe syntax.
+    Can be used as a decorator.
 
     Args:
         func: Function to wrap
@@ -94,121 +114,3 @@ def is_host_reachable(host: str, port: int, timeout: int = 3) -> bool:
     except socket.error:
         # Could not connect. Host may be down.
         return False
-
-
-def wait_for_shutdown(host: str, timeout=120) -> bool:
-    """Waits for a host to go offline.
-
-    This is often useful after triggering system power functions
-    (OS reboot, AC/DC power cycling, system resets, etc). There's
-    often a short delay between the trigger and the system actually
-    going offline.
-
-    Automation that performs such actions often relies on the connection
-    status to determine what actions to perform. Due to the initial
-    delay following a reset action, scripts that rely on pinging the host
-    to monitor the reboot status may employ a short sleep time
-    beforehand to avoid false positives.
-
-    Rather than hardcoded, arbitrary code delays, this function can be
-    used instead where accuracy of shutdown timing and execution time
-    is a concern.
-
-    Args:
-        host: Host ip address.
-        timeout: Time in seconds to wait for the system to shutdown.
-
-    Returns:
-        A boolean to indicate whether the system shutdown in the
-            expected time. False often indicates the system failed
-            to shutdown.
-    """
-    time_start = time.time()
-
-    while time.time() - time_start <= timeout:
-        # Checks ports for SSH and RDP
-        if not any([
-            is_host_reachable(host, 22),
-            is_host_reachable(host, 3389),
-        ]):
-            return True
-
-    return False
-
-
-def wait_for_boot(host: str, timeout=300) -> bool:
-    """Waits for host to go online.
-
-    Args:
-        host: Host ip address.
-        timeout: Time in seconds to wait for the system to boot.
-
-    Returns:
-        A boolean to indicate whether the system booted in the
-            expected time. False often indicates the system failed
-            to boot.
-    """
-    time_start = time.time()
-
-    while time.time() - time_start <= timeout:
-        # Checks ports for SSH and RDP
-        if any([
-            is_host_reachable(host, 22),
-            is_host_reachable(host, 3389),
-        ]):
-            return True
-
-        time.sleep(15)
-
-    return False
-
-
-def wait_for_tunnel_boot(tunnel, timeout=300) -> bool:
-    """Wait for host to go online.
-
-    Very situational, such as BMC reboot operations.
-
-    Args:
-        tunnel: Tunnel object from MagicSSH.
-        timeout: Time in seconds to wait for tunnel to boot.
-
-    Returns:
-        Boolean indicating whether the system booted in the expected
-            time.
-    """
-    time_start = time.time()
-
-    while time.time() - time_start <= timeout:
-        tunnel.check_tunnels()
-        if tunnel.tunnel_is_up.get(
-            tunnel.local_bind_address, False
-        ):
-            return True
-
-    return False
-
-
-def wait_for_tunnel_shutdown(tunnel, timeout=120):
-    """Wait for host to go online.
-
-    Very situational, such as BMC reboot operations.
-
-    Args:
-        tunnel: sshtunnel tunnel object.
-        timeout: Time in seconds to wait for tunnel to shutdown
-
-    Returns:
-        Boolean indicating whether the system shutdown in the expected
-            time.
-
-    """
-    time_start = time.time()
-
-    while time.time() - time_start <= timeout:
-        tunnel.check_tunnels()
-        if not tunnel.tunnel_is_up.get(
-            tunnel.local_bind_address, False
-        ):
-            return True
-
-    return False
